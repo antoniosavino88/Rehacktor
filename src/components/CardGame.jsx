5d3d655bf888d91c67d7f91ee001308d3d645a24
Antonio Savino <antonio.savino88@gmail.com>
2025-06-12 16:10:03 +0200

    implementati tag con link parametrici su cards

--- src/components/CardGame.jsx
+++ src/components/CardGame.jsx
@@ -1,36 +1,47 @@
 import LazyLoadGameImage from "../components/LazyLoadGameImage";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import ToggleFavorite from "./ToggleFavorite";
 import { useContext } from "react";
 import SessionContext from "../context/SessionContext";
 
 export default function CardGame({ game }) {
-  const genres = game.genres.map((genre) => genre.name).join(", ");
+  const genres = game.genres;
   const { session } = useContext(SessionContext);
 
   return (
     <div className="flex justify-center my-3">
-      <article
-        key={game.id}
-        className="bg-primary text-text rounded-2xl overflow-hidden shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-2xl max-w-sm w-3/2"
-      >
+      <article className="bg-primary text-text rounded-2xl overflow-hidden shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-2xl max-w-sm w-full">
         <LazyLoadGameImage image={game.background_image} />
 
         <div className="p-5">
-          {session ? (
+          {session && (
             <div className="flex justify-end">
               <ToggleFavorite data={game} />
             </div>
-          ) : (
-            ""
           )}
-          <h2 className="text-xl font-bold mb-1">{game.name}</h2>
-          <p className="text-sm text-gray-400 mb-2 italic">{genres}</p>
+          <h2 className="text-xl font-bold mb-2">{game.name}</h2>
+
+          <div className="flex flex-wrap gap-2 mb-4">
+            {genres.map((g) => (
+              <Link
+                key={g.id}
+                to={`/genres/${g.slug}`}
+                className="text-xs font-medium bg-slate-700 text-white px-2 py-1 rounded hover:bg-slate-600 transition"
+              >
+                {g.name}
+              </Link>
+            ))}
+          </div>
+
           <p className="text-sm mb-4">Rilasciato: {game.released}</p>
+
           <div className="flex justify-end">
-            <button className="bg-accent hover:bg-accent-hover text-primary font-semibold py-2 px-4 rounded-full transition duration-300">
-              <Link to={`/games/${game.slug}/${game.id}`}>Vai al gioco</Link>
-            </button>
+            <Link
+              to={`/games/${game.slug}/${game.id}`}
+              className="bg-accent hover:bg-accent-hover text-primary font-semibold py-2 px-4 rounded-full transition duration-300"
+            >
+              Vai al gioco
+            </Link>
           </div>
         </div>
       </article>
