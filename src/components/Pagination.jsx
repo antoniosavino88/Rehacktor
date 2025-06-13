6cf0a6f59f8a09dc102cabb2e9f8856006a6e616
Antonio Savino <antonio.savino88@gmail.com>
2025-06-13 13:58:40 +0200

    corretto bug framer-motion in Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,4 @@
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Pagination({ currentPage, hasNext, onPageChange }) {
   const scrollTop = () => {
