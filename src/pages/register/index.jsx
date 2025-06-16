6a944bc0f726b10edaa80f98d61f6dfcdad28514
Antonio Savino <antonio.savino88@gmail.com>
2025-06-16 19:24:10 +0200

    migliorato sfondo per tablet login/register

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -85,7 +85,7 @@ export default function RegisterPage() {
       <img
         src={bgLogin}
         alt="hero background"
-        className="absolute inset-0 w-full h-full object-cover lg:object-center object-right z-0"
+        className="absolute inset-0 w-full h-full object-cover md:object-center object-right z-0"
       />
       {/* Overlay sfumato: nero con leggera tinta accent color */}
       <div className="absolute inset-0 bg-gradient-to-b from-black/50 via-middle/80 to-primary backdrop-blur-sm"></div>
